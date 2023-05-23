4ea12c59e03ae26a271e688bb3aa04875e077a45
crazywawa <zxkj2021@gmail.com>
2023-05-23 09:24:38 +0800

    修改title

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,7 +96,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT4</div>
         <div className={styles["sidebar-sub-title"]}>
           你的个人助理.
         </div>
